2cc455c93f98715ac6e2f01f17a01f7058915d6e
typ0520 <php12345@163.com>
2020-11-18 15:36:58 +0800

    sign

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Web3 from 'web3';
 
 var Tx = require('ethereumjs-tx');
+const sigUtil = require('eth-sig-util')
 
 function buf2hex(buffer) {
   return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
@@ -39,10 +40,10 @@ class App extends React.Component {
   transaction = async () => {
     const web3 = this.state.web3;
     const from = this.state.userAddress;
-    //const nonce = await this.state.web3.eth.getTransactionCount(from);
-    const nonce = 149;
+    const nonce = await this.state.web3.eth.getTransactionCount(from);
+    //const nonce = 149;
 
-    const rawTx =  {
+    const rawTx = {
       nonce,
       gasPrice: web3.utils.toHex(web3.utils.toWei('100', 'gwei')),
       gasLimit: web3.utils.toHex(Number(21000)),
@@ -50,7 +51,7 @@ class App extends React.Component {
       value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
       data: '',
 
-      from, 
+      from,
       //eip155
       //chainId: 3,
     };
@@ -60,7 +61,7 @@ class App extends React.Component {
 
     web3.eth.sign(hash, from, function (err, sign) {
       if (err) return console.error(err)
-      console.log('SIGNED:' + sign);
+      console.log('transaction SIGNED: ' + sign);
 
       var r = sign.substring(2, 66);
       var s = sign.substring(66, 66 + 64);
@@ -68,7 +69,7 @@ class App extends React.Component {
       console.log('r: ' + r);
       console.log('s: ' + s);
       console.log('v: ' + v);
-  
+
       var sig = {
         r: '0x' + r,
         s: '0x' + s,
@@ -78,13 +79,13 @@ class App extends React.Component {
       if (tx._chainId > 0) {
         sig.v += tx._chainId * 2 + 8;
       }
-  
+
       console.log('sig.r: ' + sig.r);
       console.log('sig.s: ' + sig.s);
       console.log('sig.v: ' + sig.v);
-  
+
       Object.assign(tx, sig);
-  
+
       var serializedTx = tx.serialize();
       console.log('0x' + serializedTx.toString('hex'));
       alert('0x' + serializedTx.toString('hex'));
@@ -96,7 +97,7 @@ class App extends React.Component {
       let chainID = 4;
       let message = "Access zkSync account.\n\nOnly sign this message for a trusted client!";
       if (chainID !== 1) {
-          message += `\nChain ID: ${chainID}.`;
+        message += `\nChain ID: ${chainID}.`;
       }
       console.log(message);
 
@@ -112,12 +113,111 @@ class App extends React.Component {
     const from = this.state.userAddress;
     const nonce = await this.state.web3.eth.getTransactionCount(from);
 
-    web3.eth.sign('0xca4e24c83c533d3cb4099895fd40f81f3cc0bb6751ba4a3d4de24a9d667d718c', from, function (err, result) {
+    web3.eth.sign('0xb7d24532eba7a569a0c2927f4903f89b6d00f4ed78879e9b5b6c646b5f56b03d', from, function (err, result) {
       if (err) return console.error(err)
+      console.log('ethSign SIGNED:' + result)
       alert(result);
-      console.log('SIGNED:' + result)
     });
   }
+
+  // signTypedData = async () => {
+  //   const web3 = this.state.web3;
+  //   const from = this.state.userAddress;
+  //   const nonce = await this.state.web3.eth.getTransactionCount(from);
+  //   //const nonce = 149;
+
+  //   const rawTx = {
+  //     nonce,
+  //     gasPrice: web3.utils.toHex(web3.utils.toWei('100', 'gwei')),
+  //     gasLimit: web3.utils.toHex(Number(21000)),
+  //     to: '0x4611F8A6988C5B96990c74d5F8FeC69e26DC650C',
+  //     value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
+  //     data: '',
+
+  //     from,
+  //     //eip155
+  //     //chainId: 3,
+  //   };
+
+  //   console.log(rawTx);
+
+  //   var tx = new Tx(rawTx);
+  //   var hash = '0x' + buf2hex(tx.hash(false));
+  //   console.log('hash: ' + hash);
+
+  //   const msgParams = [
+  //     {
+  //       type: 'string',      // Any valid solidity type
+  //       name: 'nonce',     // Any string label you want
+  //       value: rawTx.nonce.toString() // The value to sign
+  //     },
+  //     {
+  //       type: 'string',      // Any valid solidity type
+  //       name: 'gasPrice',     // Any string label you want
+  //       value: rawTx.gasPrice.toString() // The value to sign
+  //     },
+  //     {
+  //       type: 'string',      // Any valid solidity type
+  //       name: 'gasLimit',     // Any string label you want
+  //       value: rawTx.gasLimit.toString() // The value to sign
+  //     },
+  //     {
+  //       type: 'string',      // Any valid solidity type
+  //       name: 'to',     // Any string label you want
+  //       value: rawTx.to.toString() // The value to sign
+  //     },
+  //     {
+  //       type: 'string',      // Any valid solidity type
+  //       name: 'value',     // Any string label you want
+  //       value: rawTx.value.toString() // The value to sign
+  //     },
+  //     {
+  //       type: 'string',      // Any valid solidity type
+  //       name: 'data',     // Any string label you want
+  //       value: rawTx.data.toString() // The value to sign
+  //     },
+  //   ]
+
+  //   web3.currentProvider.sendAsync({
+  //     method: 'eth_signTypedData',
+  //     params: [msgParams, from],
+  //     from: from,
+  //   }, function (err, result) {
+  //     if (err) return console.error(err)
+  //     if (result.error) {
+  //       return console.error(result.error.message)
+  //     }
+  //     var sign = result.result;
+  //     console.log('signTypedData SIGNED: ' + result.result);
+
+  //     var r = sign.substring(2, 66);
+  //     var s = sign.substring(66, 66 + 64);
+  //     var v = sign.substring(66 + 64);
+  //     console.log('r: ' + r);
+  //     console.log('s: ' + s);
+  //     console.log('v: ' + v);
+
+  //     var sig = {
+  //       r: '0x' + r,
+  //       s: '0x' + s,
+  //       v: parseInt(v, 16),
+  //     }
+
+  //     if (tx._chainId > 0) {
+  //       sig.v += tx._chainId * 2 + 8;
+  //     }
+
+  //     console.log('sig.r: ' + sig.r);
+  //     console.log('sig.s: ' + sig.s);
+  //     console.log('sig.v: ' + sig.v);
+
+  //     Object.assign(tx, sig);
+
+  //     var serializedTx = tx.serialize();
+  //     console.log('0x' + serializedTx.toString('hex'));
+  //     alert('0x' + serializedTx.toString('hex'));
+  //   })
+  // }
 
   render() {
     return (
@@ -125,15 +225,14 @@ class App extends React.Component {
         <p>当前账户: {this.state.userAddress}</p>
 
         <p>
-
-        <button onClick={e=> this.transaction()}>transaction</button>
-        <p></p>
-
-        <button onClick={e=> this.login()}>Login</button>
-        <p></p>
-        <button onClick={e=> this.ethSign()}>ethSign</button>
-        <p></p>
-
+          <button onClick={e => this.login()}>Login</button>
+          <p></p>
+          <button onClick={e => this.transaction()}>transaction</button>
+          <p></p>
+          <button onClick={e=> this.ethSign()}>ethSign</button>
+          <p></p>
+          {/* <button onClick={e => this.signTypedData()}>signTypedData</button>
+          <p></p> */}
         </p>
       </div>
     );
