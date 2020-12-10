af380ec4209a31171cbeb5e5fceb0132808730b8
typ0520 <php12345@163.com>
2020-12-10 18:05:19 +0800

    支持侧链

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 
 import Web3 from 'web3';
+const Web3PromiEvent = require('web3-core-promievent');
 
 var privateChainWeb3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
 
 var Tx = require('ethereumjs-tx');
-const sigUtil = require('eth-sig-util')
-const QuoteFactory = require('./QuoteFactory.json').abi
+
 const Hello = require('./Hello.json')
 const helloContractAddress = '0x3d7207cbd4194fa6031485e6024a9e7b1b030b7d'
 const helloContract = new privateChainWeb3.eth.Contract(Hello, helloContractAddress);
@@ -17,6 +17,88 @@ const helloPrivateChainContract = new privateChainWeb3.eth.Contract(Hello, hello
 
 function buf2hex(buffer) {
   return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+}
+
+/**
+{
+  from: '用户地址',
+  to: '合约地址'
+  data: 'encodeABI'
+} 
+*/
+function signAndSendTransaction(web3, privateChainWeb3, params) {
+  const promiEvent = Web3PromiEvent();
+  setTimeout(async () => {
+      //nonce gasPrice gas to value input
+      try {
+        const nonce = await (privateChainWeb3 || web3).eth.getTransactionCount(params.from);
+        const gasPrice = params.gasPrice || await (privateChainWeb3 || web3).eth.getGasPrice();
+        const gasLimit = await (privateChainWeb3 || web3).eth.estimateGas({
+          to: params.to,
+          data: params.data,
+        })
+        var rawTx = {
+          nonce,
+          gasPrice: web3.utils.toHex(gasPrice),
+          gasLimit: web3.utils.toHex(gasLimit),
+          to: params.to,
+          value: web3.utils.toHex(params.value || '0'),
+          data: params.data || '',
+
+          from : params.from,
+        }
+
+        if (privateChainWeb3 == null || web3 == privateChainWeb3) {
+          console.log(`哈哈哈`)
+          web3.eth.sendTransaction(rawTx)
+          .on('error', (err) => {
+            promiEvent.eventEmitter.emit('error', err);
+          })
+          .on('receipt', (receipt) => {
+              promiEvent.eventEmitter.emit('receipt', receipt);
+          })
+          .on('transactionHash', (hash) => {
+              promiEvent.eventEmitter.emit('transactionHash', hash);
+          })
+        } else {
+          var tx = new Tx(rawTx);
+          var hash = '0x' + buf2hex(tx.hash(false));
+          web3.eth.sign(hash, rawTx.from, async (err, sign) => {
+            if (err) return promiEvent.eventEmitter.emit('error', err);
+      
+            var r = sign.substring(2, 66);
+            var s = sign.substring(66, 66 + 64);
+            var v = sign.substring(66 + 64);
+            var sig = {
+              r: '0x' + r,
+              s: '0x' + s,
+              v: parseInt(v, 16),
+            }
+      
+            if (tx._chainId > 0) {
+              sig.v += tx._chainId * 2 + 8;
+            }
+            Object.assign(tx, sig);
+      
+            var serializedTx = tx.serialize();
+            //console.log('0x' + serializedTx.toString('hex'));
+            privateChainWeb3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+            .on('error', (err) => {
+              promiEvent.eventEmitter.emit('error', err);
+            })
+            .on('receipt', (receipt) => {
+                promiEvent.eventEmitter.emit('receipt', receipt);
+            })
+            .on('transactionHash', (hash) => {
+                promiEvent.eventEmitter.emit('transactionHash', hash);
+            })
+          }); 
+        }
+      } catch (e) {
+        promiEvent.eventEmitter.emit('error', e);
+      }
+  }, 0);
+  return promiEvent.eventEmitter;
 }
 
 class App extends React.Component {
@@ -163,61 +245,58 @@ class App extends React.Component {
     });
   }
 
+  // signAndSendTransaction = (web3, privateChainWeb3, contractAddress, userAddress, contractMethodPromise) => {
+  //   const promiEvent = Web3PromiEvent();
+  //   setTimeout(function() {
+  //       // promiEvent.eventEmitter.emit('done', 'Hello!');
+  //       // promiEvent.resolve('Hello!');
+
+
+  //   }, 0);
+  //   return promiEvent.eventEmitter;
+  // }
+
   transaction3 = async () => {
-    const contractMethodPromiss = helloContract.methods.setName('hehe');
-    const dataField = await contractMethodPromiss.encodeABI();
+    const dataField = await helloContract.methods.setName('haha').encodeABI();
 
-    const web3 = this.state.web3;
-    const from = this.state.userAddress;
-    const nonce = await this.state.web3.eth.getTransactionCount(from);
-    //const nonce = 149;
-
-    const rawTx = {
-      nonce,
-      gasPrice: web3.utils.toHex(web3.utils.toWei('100', 'gwei')),
-      gasLimit: web3.utils.toHex(Number(61000)),
-      to: '0x3d7207cbd4194fa6031485e6024a9e7b1b030b7d',
-      value: web3.utils.toHex(web3.utils.toWei('0', 'ether')),
+    const params = {
+      from: this.state.userAddress,
+      to: helloContractAddress,
       data: dataField,
+    }
+    signAndSendTransaction(this.state.web3, null, params)
+    .on('error', (err) => {
+      console.log('transaction3 error')
+      console.log(err)
+    })
+    .on('receipt', (receipt) => {
+        console.log('transaction3 receipt')
+        console.log(receipt)
+    })
+    .on('transactionHash', (hash) => {
+        console.log(`transaction3 transactionHash: ${hash}`)
+    });
+  }
 
-      from,
-      //eip155
-      //chainId: 3,
-    };
-    var tx = new Tx(rawTx);
-    var hash = '0x' + buf2hex(tx.hash(false));
-    console.log('hash: ' + hash);
+  transaction4 = async () => {
+    const dataField = await helloContract.methods.setName('haha').encodeABI();
 
-    web3.eth.sign(hash, from, function (err, sign) {
-      if (err) return console.error(err)
-      console.log('transaction SIGNED: ' + sign);
-
-      var r = sign.substring(2, 66);
-      var s = sign.substring(66, 66 + 64);
-      var v = sign.substring(66 + 64);
-      console.log('r: ' + r);
-      console.log('s: ' + s);
-      console.log('v: ' + v);
-
-      var sig = {
-        r: '0x' + r,
-        s: '0x' + s,
-        v: parseInt(v, 16),
-      }
-
-      if (tx._chainId > 0) {
-        sig.v += tx._chainId * 2 + 8;
-      }
-
-      console.log('sig.r: ' + sig.r);
-      console.log('sig.s: ' + sig.s);
-      console.log('sig.v: ' + sig.v);
-
-      Object.assign(tx, sig);
-
-      var serializedTx = tx.serialize();
-      console.log('0x' + serializedTx.toString('hex'));
-      alert('0x' + serializedTx.toString('hex'));
+    const params = {
+      from: this.state.userAddress,
+      to: helloPrivateChainContractAddress,
+      data: dataField,
+    }
+    signAndSendTransaction(this.state.web3, privateChainWeb3, params)
+    .on('error', (err) => {
+      console.log('transaction4 error')
+      console.log(err)
+    })
+    .on('receipt', (receipt) => {
+        console.log('transaction4 receipt')
+        console.log(receipt)
+    })
+    .on('transactionHash', (hash) => {
+        console.log(`transaction4 transactionHash: ${hash}`)
     });
   }
 
@@ -360,7 +439,9 @@ class App extends React.Component {
           <p></p>
           <button onClick={e => this.transaction2()}>transaction2</button>
           <p></p>
-          <button onClick={e => this.transaction3()}>transaction3</button>
+          <button onClick={e => this.transaction3()}>teemo单独使用ropsten</button>
+          <p></p>
+          <button onClick={e => this.transaction4()}>teemo使用ropsten和测试链</button>
           <p></p>
           <button onClick={e=> this.ethSign()}>ethSign</button>
           <p></p>
